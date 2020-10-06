cb867270999a815e4639d9650d5a7fee1c5b3653
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2020-10-06 12:56:48 +0900

    t

--- filter.py
+++ filter.py
@@ -1,55 +1,16 @@
-from konlpy.tag import Hannanum
-import nltk
-import pickle
+from PIL import Image, ImageFont, ImageDraw
+import torch
+import numpy
+from pprint import pprint
 
-pos_tagger = Hannanum()
+fnt = ImageFont.truetype("D2Coding-Ver1.3.2-20180524.ttf", 16, encoding="UTF-8")
 
-def save(classifier):
-    save_classifier = open("radarconfig.pickle", "wb")
-    pickle.dump(classifier, save_classifier)
-    save_classifier.close()
-# https://dbrang.tistory.com/1268
 
-def load():
-    classifier_f = open("radarconfig.pickle", "rb")
-    classifier = pickle.load(classifier_f)
-    classifier_f.close()
-    return classifier
-# https://dbrang.tistory.com/1268
+def normalize(t: str):
+    im = Image.new("1", (384, 16), (1,))  # White
+    dr = ImageDraw.Draw(im)
+    dr.text((0, 0), t, font=fnt, fill=(0,))
+    return numpy.asarray(numpy.float32(im.split()[0]))
 
-def tokenize(doc):
-    return ['/'.join(t) for t in pos_tagger.pos(doc)]
 
-def term_exists(doc):
-    return {'exists({})'.format(word): (word in set(doc)) for word in tokens}
-
-def getvalue(classifier, test_sentence):
-    test_docs = tokenize(test_sentence)
-    test_sent_features = {word: (word in tokens) for word in test_docs}
-    return classifier.classify(test_sent_features)
-
-train = [("카지노사이트", "neg"),
-         ("아이 적성검사 받으셨나요?", "neg"),
-         ("연세대 무료체험 떴네요~~ 아이들 있으면 마감전에 챙기세요!", "neg"),
-         ("초등학생 자녀 있으신분들 무료로 다중지능검사 받아보세요!!", "neg"),
-         ("일요일 일식있다네요", "pos"),
-         ("KSP 포럼 회칙 개정/간결안 의견수렴", "pos"),
-         ("문화어 패치 개발비화 (사진 용량 큼)", "pos"),
-         ("카페 회칙 수정안 투표", "pos")]
-
-train_docs=[]
-
-for row in train:
-    train_docs.append((tokenize(row[0]), row[1]))
-
-tokens = [t for d in train_docs for t in d[0]]
-
-train_xy = [(term_exists(d), c) for d, c in train_docs]
-
-classifier = nltk.NaiveBayesClassifier.train(train_xy)
-
-#classifier = load()
-
-classifier.show_most_informative_features()
-
-#save(classifier)
+pprint(normalize(u"가나다라ㄱㄴㄷㄹ!@#!@$#@!$@1123576854&%^|"))
