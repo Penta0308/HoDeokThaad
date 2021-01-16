2d22d1391d61b478dd476a3e762ef5a5d5527f2a
Penta0308 <38977908+Penta0308@users.noreply.github.com>
2021-01-16 10:21:42 +0900

    BERT!

--- generate.py
+++ generate.py
@@ -1,13 +1,18 @@
 import modules
 
+import os
+import shutil
 import torch
-from pytorch_transformers import BertTokenizer, BertForSequenceClassification, BertConfig
+from pytorch_transformers import BertForSequenceClassification, BertConfig
 from torch.optim import *
 import torch.nn.functional as F
 from torchtext import data
 import random
+import pickle
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = 'cpu'
 
 TEXT = data.Field(tokenize=modules.tokenizer, batch_first=True)
 LABEL = data.LabelField(dtype=torch.long)
@@ -16,10 +21,10 @@ raw_data = data.TabularDataset(path='aidb.csv', format='csv',
                                fields={'text': ('text', TEXT), 'label': ('label', LABEL)})
 
 train_data, test_data = raw_data.split(split_ratio=[0.9, 0.1], stratified=False, strata_field='label',
-                                                   random_state=random.seed(modules.SEED))
+                                       random_state=random.seed(modules.SEED))
 
 MAX_VOCAB_SIZE = 16384
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 
 TEXT.build_vocab(train_data,
                  max_size=MAX_VOCAB_SIZE,
@@ -33,7 +38,7 @@ train_iterator, test_iterator = data.BucketIterator.splits(
     batch_size=BATCH_SIZE,
     sort_key=lambda x: len(x.text),
     sort_within_batch=True,
-    device=device)
+    device=DEVICE)
 
 model = BertForSequenceClassification(
     BertConfig(
@@ -49,20 +54,28 @@ model = BertForSequenceClassification(
         num_labels=2
     )
 )
-model.to(device)
+model.to(DEVICE)
 
-print(f'모델의 파라미터 수는 {sum(p.numel() for p in model.parameters() if p.requires_grad):,} 개 입니다.')
+PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
 
-optimizer = RMSprop(model.parameters(), lr=1e-6)
+model.bert.embeddings.word_embeddings.weight.data[PAD_IDX] = torch.zeros(512)
+model.bert.embeddings.word_embeddings.weight.data[UNK_IDX] = torch.zeros(512)
+
+
+print(f'Parameter: {sum(p.numel() for p in model.parameters() if p.requires_grad):,}')
+
+optimizer = RMSprop(model.parameters(), lr=0.000005)
 
 itr = 1
-epochs = 30
+epochs = 20
 total_loss = 0
 total_len = 0
 total_correct = 0
 
-model.train()
 for epoch in range(epochs):
+    model.train()
+    print(f"Epoch {epoch + 1}/{epochs}")
     for batch in train_iterator:
         optimizer.zero_grad()
 
@@ -76,16 +89,31 @@ for epoch in range(epochs):
         total_loss += loss.item()
         loss.backward()
         optimizer.step()
-
-    print(f'[Epoch {epoch + 1}/{epochs}] Iteration {itr} -> Train Loss: {total_loss / total_len:.4f}, Accuracy: {total_correct / total_len:.3f}')
+    print(f"  Train Accuracy: {total_correct / total_len:.3f}, Loss: {total_loss / total_len:.4f}")
     total_loss = 0
     total_len = 0
     total_correct = 0
+
+    model.eval()
+    for batch in test_iterator:
+        outputs = model(batch.text, labels=batch.label)
+        loss, logits = outputs
+
+        pred = torch.argmax(F.softmax(logits, dim=1), dim=1)
+        correct = pred.eq(batch.label)
+        total_correct += correct.sum().item()
+        total_len += len(batch.label)
+
+    print(f"  Test Accuracy: {total_correct / total_len:.3f}")
+    total_len = 0
+    total_correct = 0
+
     itr += 1
 
-model.save_pretrained("data/model.pt")
+shutil.rmtree("data/model/", ignore_errors=True)
+os.mkdir("data/model/")
+model.save_pretrained("data/model/")
 
-import pickle
 output = open("data/TEXT.vocab", 'wb')
 pickle.dump(TEXT.vocab, output)
 output.close()
